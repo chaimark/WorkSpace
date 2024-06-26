b1e0521a781de6169dc5a67f56242f4880d6fb16
chai21sks <3046476712@qq.com>
2024-06-26 13:43:47 +0800

    quiz 1.0.0

--- MQTT采集终端/MQTT采集器程序(新加 MQTT MBUS 网口功能)/Interflow/ATCmd_FunctionSum.c
+++ MQTT采集终端/MQTT采集器程序(新加 MQTT MBUS 网口功能)/Interflow/ATCmd_FunctionSum.c
@@ -58,35 +58,27 @@ NetDevATCmd NetDevice_ATData[ATCMD_MAXNUMBER] = {
 };
 
 //======================================发送时的指令装载函数 ================================================//
-void easyATCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void easyATCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     copyString(OutStr.Name._char, NowATCmd.ATCmd, OutStr.MaxLen, strlen(NowATCmd.ATCmd));
 }
 // 需要替换指令，并执行简单装载的指令
-void flagChangeUartIDCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void flagChangeUartIDCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     char AtCmd[100] = {0};
-    copyString(AtCmd,NowATCmd.ATCmd,100,strlen(NowATCmd.ATCmd));
-    
-    char *pAddr = NULL;
-    if ((pAddr = myStrstr(AtCmd, "SOCKLKA", strlen(AtCmd))) != NULL)
-    {
+    copyString(AtCmd, NowATCmd.ATCmd, 100, strlen(NowATCmd.ATCmd));
+
+    char * pAddr = NULL;
+    if ((pAddr = myStrstr(AtCmd, "SOCKLKA", strlen(AtCmd))) != NULL) {
         pAddr += strlen("SOCKLKA");
-    }
-    else if ((pAddr = myStrstr(AtCmd, "MAXSK", strlen(AtCmd))) != NULL)
-    {
+    } else if ((pAddr = myStrstr(AtCmd, "MAXSK", strlen(AtCmd))) != NULL) {
         pAddr += strlen("MAXSK");
-    }
-    else if ((pAddr = myStrstr(AtCmd, "TCPSE", strlen(AtCmd))) != NULL)
-    {
+    } else if ((pAddr = myStrstr(AtCmd, "TCPSE", strlen(AtCmd))) != NULL) {
         pAddr += strlen("TCPSE");
     }
     (*pAddr) = '2'; // 修改为使用串口2
     copyString(OutStr.Name._char, AtCmd, OutStr.MaxLen, strlen(AtCmd));
 }
 // 波特率设置特殊处理
-void setDevBandCmsDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void setDevBandCmsDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     // AT+UART2=9600,8,1,NONE,NFC\r\n
     char BandData[16] = {"2000000"};
     int ArrayLen = doneBaseNumberDataToAsciiStr(BandData, 16, UART0_BAND, 16);
@@ -95,49 +87,37 @@ void setDevBandCmsDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
     sprintf(OutStr.Name._char, "AT+UART2=%s,8,1,NONE,NFC\r\n", BandData);
 }
 // 网口设置特殊处理
-void setWannCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void setWannCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     // AT+WANN=STATIC,192.168.0.7,255.255.255.0,192.168.0.1\r\n
     memset(OutStr.Name._char, 0, OutStr.MaxLen);
     sprintf(OutStr.Name._char, "AT+WANN=STATIC,%s,%s,%s\r\n", AT24CXX_Manager.NET_Local_IP, AT24CXX_Manager.NET_Local_MASK, AT24CXX_Manager.NET_Local_GATEWAY);
     int ArrayLen = strlen(OutStr.Name._char);
     OutStr.Name._char[(ArrayLen < OutStr.MaxLen) ? ArrayLen : (OutStr.MaxLen - 1)] = '\0';
 }
-void setRemoteIPCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void setRemoteIPCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     // AT+SOCKA2=TCPC,59.110.170.225,1883\r\n
     memset(OutStr.Name._char, 0, OutStr.MaxLen);
-    if (myStrstr(NowATCmd.ATCmd, "SOCKA2", strlen(NowATCmd.ATCmd)) != NULL)
-    {
+    if (myStrstr(NowATCmd.ATCmd, "SOCKA2", strlen(NowATCmd.ATCmd)) != NULL) {
         sprintf(OutStr.Name._char, "AT+SOCK2=TCPC,%s,%d\r\n", AT24CXX_Manager.NET4G_Remote_Url, AT24CXX_Manager.NET_Remote_Port);
-    }
-    else if (myStrstr(NowATCmd.ATCmd, "MQTTSER", strlen(NowATCmd.ATCmd)) != NULL)
-    {
+    } else if (myStrstr(NowATCmd.ATCmd, "MQTTSER", strlen(NowATCmd.ATCmd)) != NULL) {
         sprintf(OutStr.Name._char, "AT+MQTTSER=%s,%d\r\n", AT24CXX_Manager.NET4G_Remote_Url, AT24CXX_Manager.NET_Remote_Port);
-    }
-    else
-    {
+    } else {
         easyATCmdDataLoad(OutStr, NowATCmd);
     }
     int ArrayLen = strlen(OutStr.Name._char);
     OutStr.Name._char[(ArrayLen < OutStr.MaxLen) ? ArrayLen : (OutStr.MaxLen - 1)] = '\0';
 }
-void setUserAndPassWordDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void setUserAndPassWordDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     memset(OutStr.Name._char, 0, OutStr.MaxLen);
-    if (strcmp(NowATCmd.ATCmd, "AT+MQTTUSER=ql") == 0)
-    {
+    if (strcmp(NowATCmd.ATCmd, "AT+MQTTUSER=ql") == 0) {
         sprintf(OutStr.Name._char, "AT+MQTTUSER=%s\r\n", "ql");
-    }
-    else if (strcmp(NowATCmd.ATCmd, "AT+MQTTPSW=ql") == 0)
-    {
+    } else if (strcmp(NowATCmd.ATCmd, "AT+MQTTPSW=ql") == 0) {
         sprintf(OutStr.Name._char, "AT+MQTTPSW=%s\r\n", "ql");
     }
     int ArrayLen = strlen(OutStr.Name._char);
     OutStr.Name._char[(ArrayLen < OutStr.MaxLen) ? ArrayLen : (OutStr.MaxLen - 1)] = '\0';
 }
-void setMQTTMassagePUBCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void setMQTTMassagePUBCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     static uint8_t My_PUSName_I = 0;
     // AT+MQTTPUB=1,ON,hy/gw/get/02345678903,0,1,0,OFF,2
     memset(OutStr.Name._char, 0, OutStr.MaxLen);
@@ -147,8 +127,7 @@ void setMQTTMassagePUBCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
     int ArrayLen = strlen(OutStr.Name._char);
     OutStr.Name._char[(ArrayLen < OutStr.MaxLen) ? ArrayLen : (OutStr.MaxLen - 1)] = '\0';
 }
-void setMQTTMassageSUBCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
-{
+void setMQTTMassageSUBCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd) {
     // AT+MQTTSUB=1,ON,/hy/gw/#,0,0,&#44,2
     memset(OutStr.Name._char, 0, OutStr.MaxLen);
     sprintf(OutStr.Name._char, "AT+MQTTSUB=1,ON,/hy/gw/#,0,0,&#44,2\r\n");
@@ -158,16 +137,14 @@ void setMQTTMassageSUBCmdDataLoad(strnew OutStr, NetDevATCmd NowATCmd)
 #warning "增加网口 ATCmd 装载函数"
 
 //========================接受模组反馈的信息，判断是否发送成功，或执行其他操作===================================//
-bool easyATCmdDataDoing(NetDevATCmd NowATCmd)
-{
+bool easyATCmdDataDoing(NetDevATCmd NowATCmd) {
     if (myStrstr(Now_NetDevParameter.NetDataBuff, NowATCmd.SucessStr, Now_NetDevParameter.NetDataBuff_NowLen) != NULL)
         return true;
     else
         return false;
 }
 // 重启指令特殊处理
-bool ReBootDevATCmdDoing(NetDevATCmd NowATCmd)
-{
+bool ReBootDevATCmdDoing(NetDevATCmd NowATCmd) {
     bool ReFalg = easyATCmdDataDoing(NowATCmd);
     FL_DelayMs(1000); // 延时等待重启结束
     return ReFalg;
@@ -175,68 +152,61 @@ bool ReBootDevATCmdDoing(NetDevATCmd NowATCmd)
 #warning "增加网口 ATCmd 处理函数"
 
 // 设置或查询 AT指令 的装载方法
-void _DataInstallation(strnew OutStr, struct _NetDevATCmdData This)
-{
+void _DataInstallation(strnew OutStr, struct _NetDevATCmdData This) {
     // 调用实际装载的函数
-    switch (This.LoadATSendFunName)
-    {
-    case flagChangeUartIDFun:
-        flagChangeUartIDCmdDataLoad(OutStr, This);
-        break;
-    case easyATCmdDataLoadFun:
-        easyATCmdDataLoad(OutStr, This);
-        break;
-    case setBandDataLoadFun:
-        setDevBandCmsDataLoad(OutStr, This);
-        break;
-    case setWannDataLoadFun:
-        setWannCmdDataLoad(OutStr, This);
-        break;
-    case setRemoteIPDataLoadFun:
-        setRemoteIPCmdDataLoad(OutStr, This);
-        break;
-    case setUserAndPassWordDataLoadFun:
-        setUserAndPassWordDataLoad(OutStr, This);
-        break;
-    case setMQTTMassagePUBFun:
-        setMQTTMassagePUBCmdDataLoad(OutStr, This);
-        break;
-    case setMQTTMassageSUBFun:
-        setMQTTMassageSUBCmdDataLoad(OutStr, This);
-        break;
+    switch (This.LoadATSendFunName) {
+        case flagChangeUartIDFun:
+            flagChangeUartIDCmdDataLoad(OutStr, This);
+            break;
+        case easyATCmdDataLoadFun:
+            easyATCmdDataLoad(OutStr, This);
+            break;
+        case setBandDataLoadFun:
+            setDevBandCmsDataLoad(OutStr, This);
+            break;
+        case setWannDataLoadFun:
+            setWannCmdDataLoad(OutStr, This);
+            break;
+        case setRemoteIPDataLoadFun:
+            setRemoteIPCmdDataLoad(OutStr, This);
+            break;
+        case setUserAndPassWordDataLoadFun:
+            setUserAndPassWordDataLoad(OutStr, This);
+            break;
+        case setMQTTMassagePUBFun:
+            setMQTTMassagePUBCmdDataLoad(OutStr, This);
+            break;
+        case setMQTTMassageSUBFun:
+            setMQTTMassageSUBCmdDataLoad(OutStr, This);
+            break;
     }
 }
 // 处理指令返回的方法
-bool _DoingATCmdResData(struct _NetDevATCmdData This)
-{
+bool _DoingATCmdResData(struct _NetDevATCmdData This) {
     bool ResFlag = false;
     // 调用实际处理回复的函数
-    switch (This.DoingATReceiveFunName)
-    {
-    case ResetTrueFlagDoingFun:
-        ResFlag = true;
-        break;
-    case easyATCmdDataDoingFun:
-        ResFlag = easyATCmdDataDoing(This);
-        break;
-    case ReBootDevFun:
-        ResFlag = ReBootDevATCmdDoing(This);
-        break;
+    switch (This.DoingATReceiveFunName) {
+        case ResetTrueFlagDoingFun:
+            ResFlag = true;
+            break;
+        case easyATCmdDataDoingFun:
+            ResFlag = easyATCmdDataDoing(This);
+            break;
+        case ReBootDevFun:
+            ResFlag = ReBootDevATCmdDoing(This);
+            break;
     }
     return ResFlag;
 }
 // 建立对象的函数
-NetDevATCmd New_NetDevAT_Obj(NetDevATCmd DataInit)
-{
+NetDevATCmd New_NetDevAT_Obj(NetDevATCmd DataInit) {
     DataInit.DataInstallation = _DataInstallation;
     DataInit.DoingATCmdResData = _DoingATCmdResData;
     return DataInit;
 }
 
-void MQTT_NetAT_Init(void)
-{
-    for (int i = 0; i < ATCMD_MAXNUMBER; i++)
-    {
+void MQTT_NetAT_Init(void) {
+    for (int i = 0; i < ATCMD_MAXNUMBER; i++) {
         NetDevice_ATData[i] = New_NetDevAT_Obj(NetDevice_ATData[i]);
     }
     return;
--- MQTT采集终端/MQTT采集器程序(新加 MQTT MBUS 网口功能)/Interflow/ATCmd_FunctionSum.h
+++ MQTT采集终端/MQTT采集器程序(新加 MQTT MBUS 网口功能)/Interflow/ATCmd_FunctionSum.h
@@ -8,9 +8,8 @@
 #define sendDataByNetProt Uart0Send // Uart0Send(unsigned char *, unsigned short int)
 
 // 所有 AT指令 《发送时》 需要的用于 《组织指令》 的函数名
-typedef enum _SendFun
-{                                      // 发送AT指令时调用什么函数发送，对应的函数名
-    flagChangeUartIDFun = 0,              // 控制修改 AT 指令需要的串口号
+typedef enum _SendFun {                                      // 发送AT指令时调用什么函数发送，对应的函数名
+    flagChangeUartIDFun = 0,           // 控制修改 AT 指令需要的串口号
     easyATCmdDataLoadFun = 1,          // 简单AT 指令装载
     setBandDataLoadFun = 2,            // 波特率装载设置
     setWannDataLoadFun = 3,            // 网口设置指令装载
@@ -21,20 +20,18 @@ typedef enum _SendFun
 } SendFun;
 
 // 所有 AT指令 《回复时》 需要的用于 《处理回复》 的函数名
-typedef enum _receiveFun
-{                              // 发送AT指令时调用什么函数发送，对应的函数名
+typedef enum _receiveFun {                              // 发送AT指令时调用什么函数发送，对应的函数名
     ResetTrueFlagDoingFun = 0, // 作为特殊标记，用于忽略模组返回强制跳过某条指令
     easyATCmdDataDoingFun = 1, // 简单AT 指令处理
     ReBootDevFun = 2,          // 重启命令特殊处理
 } receiveFun;
 
 // 类定义
-typedef struct _NetDevATCmdData
-{                         // 成员
+typedef struct _NetDevATCmdData {                         // 成员
     int8_t CmdID;         // 指令ID
     int8_t Next_CmdID;    // 下一条指令ID
-    char *ATCmd;          // AT指令
-    char *SucessStr;      // 成功返回的字符串
+    char * ATCmd;          // AT指令
+    char * SucessStr;      // 成功返回的字符串
     bool RunFlag;         // 是否执行当前指令
     uint8_t CmsResCount;  // 回复查询次数限制
     uint8_t CmdSendCount; // 指令发送次数限制
