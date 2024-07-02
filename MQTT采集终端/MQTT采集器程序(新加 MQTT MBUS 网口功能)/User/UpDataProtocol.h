efeb1a98214ddfbd4455bfa1473e6881a0d9f15c
chai21sks <3046476712@qq.com>
2024-07-02 09:10:46 +0800

    v5.0.3

--- MQTT采集终端/MQTT采集器程序(新加 MQTT MBUS 网口功能)/User/UpDataProtocol.h
+++ MQTT采集终端/MQTT采集器程序(新加 MQTT MBUS 网口功能)/User/UpDataProtocol.h
@@ -4,7 +4,7 @@
 #include "main.h"
 #include "AT24C0256.h"
 
-#define CONSOLE_UART  UART5
+#define CONSOLE_UART UART5
 #define CONSOLE_BUF_SIZE 1024  
 #define CONSOLE_NEED_BUF       // 需要命令缓存，上下可切换  // 640 Byte
 #define CONSOLE_BUF_NUM    10  // 缓存条数
@@ -12,9 +12,9 @@
 #define CONSOLE_PAR_MAX_NUM  3 // 最大参数数量
 
 typedef struct {
-	char const* cmd;
-	void (*handle)(int argc, char* argv[]);
-	char const* help;
+    char const * cmd;
+    void (*handle)(int argc, char * argv[]);
+    char const * help;
 }console_cmd_list_t;
 extern void UpDataProtocol_Timer_Add(void);
 #endif 
